7d7356c67c507fd0c86df61730e6026b037725d2
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-17 17:46:37 +0100

    Add files via upload

---
+++ colors.js
@@ -0,0 +1,27 @@
+const colors=["yellow", "blue", "red", "orange"];
+
+// var i = 0;
+// while (i < colors.length) {
+//   console.log("The color is", colors[i])
+//   i++;
+// }
+
+
+// var i = 0;
+// for (let i = 0; i < colors.length; i++) {
+//   console.log("The color is", colors[i])
+//   ;
+// }
+
+// colors.forEach(element => console.log(element));
+
+const dierenObject = {
+    kat: "miauw",
+    hond: "waf waf",
+    vis: "blub",
+    koe: "moe",
+    kip: "tok",
+  };
+for (alle in dierenObject) {
+console.log(dierenObject[alle]);
+}
